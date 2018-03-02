c722d6375f80632a1c4503ff7ce62f433a88c072
pzl <dan@panzarel.la>
2018-03-02 14:18:31 -0500

    ingest script listens on socket now

--- scingest/scingest.py
+++ scingest/scingest.py
@@ -2,6 +2,12 @@
 
 import sane
 from PIL import TiffImagePlugin
+import logging
+import socket
+import json
+from signal import signal, SIGTERM, SIGINT
+import atexit
+import sys
 
 """
 Interesting options:
@@ -25,10 +31,51 @@ Interesting options:
     cover_open: 0/1
 """
 
-class Feed(object):
+logging.basicConfig(level=logging.DEBUG)
+
+class Client(object):
+    """Communication with sccontrol client"""
+    def __init__(self, sock, addr):
+        super(Client, self).__init__()
+        self.sock = sock
+        self.addr = addr
+        atexit.register(self.cleanup)
+        logging.info("got connection from {}".format(addr))
+
+    def cleanup(self):
+        logging.debug("cleaning up client socket")
+        self.sock.close()
+
+    def _getmsg(self):
+        pkt_len = self.sock.recv(4)
+        if pkt_len == b'':
+            return None
+        pkt_len = int.from_bytes(pkt_len, byteorder='big')
+        nbytes=0
+        chunks=[]
+        while nbytes < pkt_len:
+            chunk = self.sock.recv(min(pkt_len - nbytes, 2048))
+            if chunk == b'':
+                return None
+            chunks.append(chunk)
+            nbytes += len(chunk)
+        return b''.join(chunks)
+
+
+    def _get_command(self):
+        msg = self._getmsg()
+        return json.loads(msg.decode('utf8'))
+
+    def process(self, cb):
+        # client should contact us first with options
+        opts = self._get_command()
+        cb(opts)
+
+
+class PageFeed(object):
     """Page iterator for ADF feed since python-sane doesn't do it right for python3"""
     def __init__(self, dev):
-        super(Feed, self).__init__()
+        super(PageFeed, self).__init__()
         self.dev = dev
     def __iter__(self):
         return self
@@ -44,43 +91,104 @@ class Feed(object):
                 raise
         return self.dev.snap(True)
 
-def get_fujitsu():
-    devs = sane.get_devices()
-    return sane.open(next(x for x in devs if x[1] == 'FUJITSU')[0])
+class Scanner(object):
+    """SANE communication with scanner"""
 
-def set_defaults(fj):
-    fj.source='ADF Duplex'
-    fj.mode='Color'
-    fj.resolution=300
-    fj.ald=1
-    fj.swskip=15.0
-    fj.swcrop=1
-    fj.swdeskew=1
-    fj.swdespeck=1
+    # Scanner default options
+    defaults={
+        'source':'ADF Duplex',
+        'mode':'Color',
+        'resolution':300,
+        'ald':1,
+        'swskip':15.0,
+        'swcrop':1,
+        'swdeskew':1,
+        'swdespeck':1,
+    }
 
+    def __init__(self):
+        super(Scanner, self).__init__()
+        self.handle=None
+        sane.init()
+        self.get_device('FUJITSU')
+        logging.debug('got scanning device')
+        atexit.register(self.cleanup)
 
-def scan(device):
-    """ This was a bit unreliable, and giving 0 when paper was fully loaded
-    if not device.page_loaded:
-        print("no pages")
-        return
-    """
-    feed = Feed(device)
-    with TiffImagePlugin.AppendingTiffWriter("out.tiff") as tiff:
-        for i,page in enumerate(feed):
-            print('reading page {}...'.format(i))
-            page.save(tiff)
-            tiff.newFrame()
-    print("done")
+    def get_device(self, manufac):
+        devs = sane.get_devices()
+        self.device = [x for x in devs if x[1] == manufac][0][0]
 
+    def connect(self):
+        self.handle = sane.open(self.device)
+        logging.debug("Connected to {}".format(self.device))
+
+    def disconnect(self):
+        if self.handle:
+            logging.debug("Closing handle to {}".format(self.device))
+            self.handle.close()
+            self.handle=None
+
+    def setopts(self, device, options):
+        settings = {**self.defaults, **options}
+        for opt,val in settings:
+            setattr(device,opt,val)
+
+    def scanwrite(self, feed, filename):
+        with TiffImagePlugin.AppendingTiffWriter(filename) as tiff:
+            for i,page in enumerate(feed):
+                logging.info('reading page {}...'.format(i))
+                page.save(tiff)
+                tiff.newFrame
+
+    # called as callback in Client socket processing
+    def scan(self, options):
+        logging.info('scan starting')
+        device = self.connect()
+        self.setopts(device, options)
+        pages = PageFeed(device)
+        self.scanwrite(pages, "out.tiff")
+        logging.info('scan complete')
+        self.disconnect()
+
+    def cleanup(self):
+        logging.info('cleaning up SANE')
+        self.disconnect()
+        sane.exit()
+
+class Server(object):
+    """Socket listener"""
+    def __init__(self, port):
+        super(Server, self).__init__()
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind(('',port))
+        self.launch = lambda x: pass
+        logging.info('Server listening at port {}'.format(port))
+        atexit.register(self.cleanup)
+
+    def cleanup(self):
+        logging.debug('Server closing socket')
+        self.socket.close()
+
+    def onconnect(self, cb):
+        self.launch = cb
+
+    def listen(self):
+        self.socket.listen(5)
+        while True:
+            client = Client(*self.socket.accept())
+            client.process(cb)
+
+def cleanup_at_exit():
+    signal(SIGTERM, lambda signum, stack_frame: sys.exit(1))
+    signal(SIGINT, lambda signum, stack_frame: sys.exit(1))
 
 def main():
-    sane.init()
-    devs = sane.get_devices()
-    fj = get_fujitsu()
-    set_defaults(fj)
-    scan(fj)
-    fj.close()
+    cleanup_at_exit()
+    scanner = Scanner()
+    server = Server(5555)
+    server.onconnect(scanner.scan)
+    server.listen()
 
 if __name__ == "__main__":
     main()
